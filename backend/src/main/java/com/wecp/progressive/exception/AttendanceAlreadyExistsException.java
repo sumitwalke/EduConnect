cfb687f22031e4fbbd513f667ea3c28c2d91ab5e
username <wecp-dummy@gmail.com>
2025-09-11 16:48:08 +0000

    Day 16

--- backend/src/main/java/com/wecp/progressive/exception/AttendanceAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.wecp.progressive.exception;
-
-public class AttendanceAlreadyExistsException extends RuntimeException{
-    public AttendanceAlreadyExistsException(String msg){
-        super(msg);
-    }
-}
--- backend/src/main/java/com/wecp/progressive/exception/EnrollmentAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.wecp.progressive.exception;
-
-public class EnrollmentAlreadyExistsException extends RuntimeException{
-    public EnrollmentAlreadyExistsException(String msg){
-        super(msg);
-    }
-}
